6e8a69504e3c5d578138b05ea9b65d630b2ac121
Nicholas <nicholas@flightlogger.net>
2021-06-22 19:45:06 +0200

    .

--- gruntfile.js
+++ gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
                 //server: 'season'
             },
             dist: {
-                src: ['../main/*.js']
+                src: ['src/*.js']
             }
         }
     });
