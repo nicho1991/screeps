7a2493ee956c1673fbb935b4973c8fbf9ad48b96
Nicholas <nicholas@flightlogger.net>
2021-06-22 21:38:06 +0200

    dynamic harverster allokation

--- src/main.js
+++ src/main.js
@@ -1,38 +1,43 @@
-var moveToSource = (creep) => {
-    var sources = creep.room.find(FIND_SOURCES);
-    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
-    }
-};
 
-var moveToSpawn = (creep) => {
-    if( creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
-        creep.moveTo(Game.spawns['Spawn1']);
-    }
-};
-
-function GetCreepsByRole(role){
-    var CreepList = [];
-      for (var creepname in Game.creeps){
-        if (Game.creeps[creepname].memory.role == role){
-        CreepList.push(Game.creeps[creepname]);
-      }
-    }
-    return CreepList
-  }
 
 module.exports.loop = function () {
+    var moveToSource = (creep) => {
+        var source = Game.getObjectById(creep.memory.source)
+        if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+        }
+    };
+    
+    var moveToSpawn = (creep) => {
+        if( creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
+            creep.moveTo(Game.spawns['Spawn1'], {visualizePathStyle: {stroke: '#ffaa00'}});
+        }
+    };
+    
+    function GetCreepsByRole(role){
+        var CreepList = [];
+            for (var creepname in Game.creeps){
+            if (Game.creeps[creepname].memory.role == role){
+            CreepList.push(Game.creeps[creepname]);
+            }
+        }
+        return CreepList
+    }
     require('version')
     if(!Memory.SCRIPT_VERSION || Memory.SCRIPT_VERSION != SCRIPT_VERSION) {
         Memory.SCRIPT_VERSION = SCRIPT_VERSION
         console.log('New code uplodated')
     }
 
-    var harvesters = GetCreepsByRole('harvester')
-    if (harvesters.length < 1) {  // spawn harvester if none left
-        console.log('spawning harvester')
-        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Worker1', {
-            memory: {role: 'harvester'}
+    var harvesters = GetCreepsByRole("harvester")
+    var spawn = Game.spawns["Spawn1"]
+    var listSources = spawn.room.find(FIND_SOURCES)
+    if (harvesters.length < listSources.length) {  // spawn harvester if none left
+        var havesterNumber = harvesters.length
+        console.log('spawning harvester: ' + havesterNumber)
+
+        spawn.spawnCreep([WORK, CARRY, MOVE], 'Harvester' + havesterNumber, {
+            memory: {role: 'harvester', source: listSources[havesterNumber % listSources.length].id}
         });
     }
 
